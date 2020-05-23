8967b941b8e55954e59b136e2a647a4d91183f4f
Thraxus <mornadak@hotmail.com>
2020-05-23 06:35:13 -0400

    1.6 release

--- AwwScrap/AwwScrap/Data/Scripts/Thraxus/AwwScrapSession.cs
+++ AwwScrap/AwwScrap/Data/Scripts/Thraxus/AwwScrapSession.cs
@@ -1,109 +1,132 @@
-﻿using System.Linq;
-using AwwScrap.Helpers;
-using AwwScrap.Utilities;
+﻿using System;
+using System.Linq;
 using Sandbox.Definitions;
 using Sandbox.ModAPI;
 using VRage;
 using VRage.Game;
 using VRage.Game.Components;
 using AwwScrap.Support;
+using Sandbox.Common.ObjectBuilders;
+using VRage.Utils;
 
 namespace AwwScrap
 {
 	[MySessionComponentDescriptor(MyUpdateOrder.NoUpdate)]
 	public class AwwScrapSession : MySessionComponentBase
 	{
-		public static bool LogSetupComplete;
-		public static Log GeneralLog;
-		
+		public override void LoadData()
+		{
+			base.LoadData();
+			SetSurvivalKitMenu();
+		}
+
 		public override void BeforeStart()
 		{
 			base.BeforeStart();
 			Initialize();
 		}
-
-		protected override void UnloadData()
-		{
-			CloseLogs();
-		}
-
-		private static void InitLogs()
-		{
-			if (Constants.EnableGeneralLog) GeneralLog = new Log(Constants.GeneralLogName);
-			LogSetupComplete = true;
-		}
-
-		private static void CloseLogs()
-		{
-			if (Constants.EnableGeneralLog) GeneralLog.Close();
-		}
-
-
+		
 		private static void Initialize()
 		{
-			InitLogs();
 			MyAPIGateway.Parallel.StartBackground(ScrubCubes);
 			MyAPIGateway.Parallel.StartBackground(SetEfficiency);
 			MyAPIGateway.Parallel.StartBackground(SetAttributes);
 		}
 
-
-
 		private static void ScrubCubes()
 		{
-			MyPhysicalItemDefinition scrapDef = MyDefinitionManager.Static.GetPhysicalItemDefinition(new MyDefinitionId(typeof(MyObjectBuilder_Ore), "Scrap"));
-			
-			foreach (MyCubeBlockDefinition myCubeBlockDefinition in MyDefinitionManager.Static.GetAllDefinitions().OfType<MyCubeBlockDefinition>().Where(myCubeBlockDefinition => myCubeBlockDefinition?.Components != null))
+			try
 			{
-				foreach (MyCubeBlockDefinition.Component component in myCubeBlockDefinition.Components)
+				MyPhysicalItemDefinition scrapDef = MyDefinitionManager.Static.GetPhysicalItemDefinition(new MyDefinitionId(typeof(MyObjectBuilder_Ore), "Scrap"));
+			
+				foreach (MyCubeBlockDefinition myCubeBlockDefinition in MyDefinitionManager.Static.GetAllDefinitions().OfType<MyCubeBlockDefinition>().Where(myCubeBlockDefinition => myCubeBlockDefinition?.Components != null))
 				{
-					//GeneralLog.WriteToLog("ScrubCubes", $"{component.Definition.Id.SubtypeId}");
-
-					if (!component.Definition.Public)
-						continue;
-
-					string subtypeName;
-					if (Statics.ComponentDictionary.TryGetValue(component.Definition.Id.SubtypeId, out subtypeName))
+					foreach (MyCubeBlockDefinition.Component component in myCubeBlockDefinition.Components)
 					{
-						component.DeconstructItem = MyDefinitionManager.Static.GetPhysicalItemDefinition(new MyDefinitionId(typeof(MyObjectBuilder_Ore), subtypeName));
-						continue;
-					}
-
-					if (Statics.SkipTieredTech)
-						if (component.Definition.Id.SubtypeId.ToString() == "Tech2x" || component.Definition.Id.SubtypeId.ToString() == "Tech4x" || component.Definition.Id.SubtypeId.ToString() == "Tech8x")
+						if (!component.Definition.Public)
 							continue;
-					component.DeconstructItem = scrapDef;
+
+						string subtypeName;
+						if (Statics.ComponentDictionary.TryGetValue(component.Definition.Id.SubtypeId, out subtypeName))
+						{
+							component.DeconstructItem = MyDefinitionManager.Static.GetPhysicalItemDefinition(new MyDefinitionId(typeof(MyObjectBuilder_Ore), subtypeName));
+							continue;
+						}
+
+						if (Statics.SkipTieredTech)
+							if (component.Definition.Id.SubtypeId.ToString() == "Tech2x" || component.Definition.Id.SubtypeId.ToString() == "Tech4x" || component.Definition.Id.SubtypeId.ToString() == "Tech8x")
+								continue;
+						component.DeconstructItem = scrapDef;
+					}
 				}
+			}
+			catch (Exception e)
+			{
+				MyLog.Default.WriteLine($"AwwScrap: ScrubCubes - Boom!!! {e}");
 			}
 		}
 
 		private static void SetAttributes()
 		{
-			foreach (MyPhysicalItemDefinition item in MyDefinitionManager.Static.GetPhysicalItemDefinitions())
+			try
 			{
-				ScrapAttributes scrap;
-				if (!Statics.ScrapAttributesDictionary.TryGetValue(item.Id.SubtypeId, out scrap))
-					continue;
-				//GeneralLog.WriteToLog("SetAttributes", $"Attributes for {item.Id.SubtypeId} are currently Mass: {item.Mass} - Volume: {item.Volume}");
-				item.Mass = scrap.Mass;
-				item.Volume = scrap.Volume / 1000;
-				//GeneralLog.WriteToLog("SetAttributes", $"Attributes for {item.Id.SubtypeId} are now Mass: {item.Mass} - Volume: {item.Volume}");
+				foreach (MyPhysicalItemDefinition item in MyDefinitionManager.Static.GetPhysicalItemDefinitions())
+				{
+					ScrapAttributes scrap;
+					if (!Statics.ScrapAttributesDictionary.TryGetValue(item.Id.SubtypeId, out scrap))
+						continue;
+					item.Mass = scrap.Mass;
+					item.Volume = scrap.Volume / 1000;
+				}
+			}
+			catch (Exception e)
+			{
+				MyLog.Default.WriteLine($"AwwScrap: SetAttributes - Boom!!! {e}");
 			}
 		}
 
 		private static void SetEfficiency()
 		{
-			// This loop accounts for World Settings for the Assembler Efficiency Modifier (x1, x3, x10)
-			foreach (MyBlueprintDefinitionBase myBlueprintDefinitionBase in Statics.AwwScrapSubTypeIds.Select(
-				subtype => MyDefinitionManager.Static.GetBlueprintDefinition(
-					new MyDefinitionId(typeof(MyObjectBuilder_BlueprintDefinition), subtype))))
+			try
 			{
-				for (int index = 0; index < myBlueprintDefinitionBase.Results.Length; index++)
-				{   // MyFixedPoint can't do /= operations, so have to do a work around
-					float f = (float)myBlueprintDefinitionBase.Results[index].Amount;
-					f /= MyAPIGateway.Session.SessionSettings.AssemblerEfficiencyMultiplier;
-					myBlueprintDefinitionBase.Results[index].Amount = (MyFixedPoint)f;
+				// This loop accounts for World Settings for the Assembler Efficiency Modifier (x1, x3, x10)
+				foreach (MyBlueprintDefinitionBase myBlueprintDefinitionBase in Statics.AwwScrapSubTypeIds.Select(
+					subtype => MyDefinitionManager.Static.GetBlueprintDefinition(
+						new MyDefinitionId(typeof(MyObjectBuilder_BlueprintDefinition), subtype))))
+				{
+					for (int index = 0; index < myBlueprintDefinitionBase.Results.Length; index++)
+					{   // MyFixedPoint can't do /= operations, so have to do a work around
+						float f = (float)myBlueprintDefinitionBase.Results[index].Amount;
+						f /= MyAPIGateway.Session.SessionSettings.AssemblerEfficiencyMultiplier;
+						myBlueprintDefinitionBase.Results[index].Amount = (MyFixedPoint)f;
+					}
 				}
+				MyRefineryDefinition basicRefinery = MyDefinitionManager.Static.GetCubeBlockDefinition(new MyDefinitionId(typeof(MyObjectBuilder_Refinery), "Blast Furnace")) as MyRefineryDefinition;
+				if (basicRefinery != null) basicRefinery.MaterialEfficiency = 1.0f;
+			}
+			catch (Exception e)
+			{
+				MyLog.Default.WriteLine($"AwwScrap: SetEfficiency - Boom!!! {e}");
+			}
+		}
+
+		private static void SetSurvivalKitMenu()
+		{
+			try
+			{
+				MyBlueprintClassDefinition awwScrap = MyDefinitionManager.Static.GetBlueprintClass("AwwScrap");
+				foreach (MyCubeBlockDefinition sKitDef in MyDefinitionManager.Static.GetAllDefinitions().OfType<MyCubeBlockDefinition>().Where(myCubeBlockDefinition => myCubeBlockDefinition is MySurvivalKitDefinition))
+				{
+					((MySurvivalKitDefinition)sKitDef).BlueprintClasses.Add(awwScrap);
+					foreach (string x in Statics.SKitScrapList)
+					{
+						((MySurvivalKitDefinition)sKitDef).InputInventoryConstraint.Add(new MyDefinitionId(typeof(MyObjectBuilder_Ore), x));
+					}
+				}
+			}
+			catch (Exception e)
+			{
+				MyLog.Default.WriteLine($"AwwScrap: SetSurvivalKitMenu - Boom!!! {e}");
 			}
 		}
 	}
--- AwwScrap/AwwScrap/Data/Scripts/Thraxus/Support/Statics.cs
+++ AwwScrap/AwwScrap/Data/Scripts/Thraxus/Support/Statics.cs
@@ -8,30 +8,52 @@ namespace AwwScrap.Support
 		public static readonly Dictionary<MyStringHash, ScrapAttributes> ScrapAttributesDictionary = new Dictionary<MyStringHash, ScrapAttributes>
 		{
 			// Vanilla
-			{ MyStringHash.GetOrCompute("BulletproofGlassScrap"), new ScrapAttributes(11.25f, 4.82f) },
-			{ MyStringHash.GetOrCompute("ComputerScrap"), new ScrapAttributes(0.52f, 0.11f) },
-			{ MyStringHash.GetOrCompute("ConstructionScrap"), new ScrapAttributes(6f, 0.77f) },
-			{ MyStringHash.GetOrCompute("DetectorScrap"), new ScrapAttributes(15f, 1.73f) },
-			{ MyStringHash.GetOrCompute("DisplayScrap"), new ScrapAttributes(4.5f, 1.7f) },
-			{ MyStringHash.GetOrCompute("ExplosivesScrap"), new ScrapAttributes(1.87f, 1.02f) },
-			{ MyStringHash.GetOrCompute("GirderScrap"), new ScrapAttributes(4.5f, 0.57f) },
-			{ MyStringHash.GetOrCompute("GravityGeneratorScrap"), new ScrapAttributes(626.25f, 76.38f) },
-			{ MyStringHash.GetOrCompute("InteriorPlateScrap"), new ScrapAttributes(2.25f, 0.29f) },
-			{ MyStringHash.GetOrCompute("LargeTubeScrap"), new ScrapAttributes(22.5f, 2.86f) },
-			{ MyStringHash.GetOrCompute("MedicalScrap"), new ScrapAttributes(112.5f, 13.02f) },
-			{ MyStringHash.GetOrCompute("MetalGridScrap"), new ScrapAttributes(15f, 1.82f) },
-			{ MyStringHash.GetOrCompute("MotorScrap"), new ScrapAttributes(18.75f, 2.33f) },
-			{ MyStringHash.GetOrCompute("PowerCellScrap"), new ScrapAttributes(9.75f, 1.44f) },
-			{ MyStringHash.GetOrCompute("RadioCommunicationScrap"), new ScrapAttributes(6.75f, 1.08f) },
-			{ MyStringHash.GetOrCompute("ReactorScrap"), new ScrapAttributes(30f, 7.34f) },
-			{ MyStringHash.GetOrCompute("SmallTubeScrap"), new ScrapAttributes(3.75f, 0.48f) },
-			{ MyStringHash.GetOrCompute("SolarCellScrap"), new ScrapAttributes(6.75f, 2.12f) },
-			{ MyStringHash.GetOrCompute("SteelPlateScrap"), new ScrapAttributes(15.75f, 2f) },
-			{ MyStringHash.GetOrCompute("SuperconductorScrap"), new ScrapAttributes(9f, 1.03f) },
-			{ MyStringHash.GetOrCompute("ThrustScrap"), new ScrapAttributes(31.05f, 3.75f) },
+			{ MyStringHash.GetOrCompute("BulletproofGlassScrap"), new ScrapAttributes(10.5f, 5.6f) },
+			{ MyStringHash.GetOrCompute("ComputerScrap"), new ScrapAttributes(0.14f, 0.7f) },
+			{ MyStringHash.GetOrCompute("ConstructionScrap"), new ScrapAttributes(5.6f, 1.4f) },
+			{ MyStringHash.GetOrCompute("DetectorScrap"), new ScrapAttributes(3.5f, 4.2f) },
+			{ MyStringHash.GetOrCompute("DisplayScrap"), new ScrapAttributes(5.6f, 4.2f) },
+			{ MyStringHash.GetOrCompute("ExplosivesScrap"), new ScrapAttributes(1.4f, 1.4f) },
+			{ MyStringHash.GetOrCompute("GirderScrap"), new ScrapAttributes(4.2f, 1.4f) },
+			{ MyStringHash.GetOrCompute("GravityGeneratorScrap"), new ScrapAttributes(560f, 140f) },
+			{ MyStringHash.GetOrCompute("InteriorPlateScrap"), new ScrapAttributes(2.1f, 3.5f) },
+			{ MyStringHash.GetOrCompute("LargeTubeScrap"), new ScrapAttributes(17.5f, 26.6f) },
+			{ MyStringHash.GetOrCompute("MedicalScrap"), new ScrapAttributes(105f, 112f) },
+			{ MyStringHash.GetOrCompute("MetalGridScrap"), new ScrapAttributes(4.2f, 10.5f) },
+			{ MyStringHash.GetOrCompute("MotorScrap"), new ScrapAttributes(16.8f, 5.6f) },
+			{ MyStringHash.GetOrCompute("PowerCellScrap"), new ScrapAttributes(17.5f, 28f) },
+			{ MyStringHash.GetOrCompute("RadioCommunicationScrap"), new ScrapAttributes(5.6f, 49f) },
+			{ MyStringHash.GetOrCompute("ReactorScrap"), new ScrapAttributes(17.5f, 5.6f) },
+			{ MyStringHash.GetOrCompute("SmallTubeScrap"), new ScrapAttributes(2.8f, 1.4f) },
+			{ MyStringHash.GetOrCompute("SolarCellScrap"), new ScrapAttributes(4.2f, 8.4f) },
+			{ MyStringHash.GetOrCompute("SteelPlateScrap"), new ScrapAttributes(14f, 2.1f) },
+			{ MyStringHash.GetOrCompute("SuperconductorScrap"), new ScrapAttributes(10.5f, 5.6f) },
+			{ MyStringHash.GetOrCompute("ThrustScrap"), new ScrapAttributes(28f, 7f) },
+
+			//{ MyStringHash.GetOrCompute("BulletproofGlassScrap"), new ScrapAttributes(11.25f, 4.82f) },
+			//{ MyStringHash.GetOrCompute("ComputerScrap"), new ScrapAttributes(0.52f, 0.11f) },
+			//{ MyStringHash.GetOrCompute("ConstructionScrap"), new ScrapAttributes(6f, 0.77f) },
+			//{ MyStringHash.GetOrCompute("DetectorScrap"), new ScrapAttributes(15f, 1.73f) },
+			//{ MyStringHash.GetOrCompute("DisplayScrap"), new ScrapAttributes(4.5f, 1.7f) },
+			//{ MyStringHash.GetOrCompute("ExplosivesScrap"), new ScrapAttributes(1.87f, 1.02f) },
+			//{ MyStringHash.GetOrCompute("GirderScrap"), new ScrapAttributes(4.5f, 0.57f) },
+			//{ MyStringHash.GetOrCompute("GravityGeneratorScrap"), new ScrapAttributes(626.25f, 76.38f) },
+			//{ MyStringHash.GetOrCompute("InteriorPlateScrap"), new ScrapAttributes(2.25f, 0.29f) },
+			//{ MyStringHash.GetOrCompute("LargeTubeScrap"), new ScrapAttributes(22.5f, 2.86f) },
+			//{ MyStringHash.GetOrCompute("MedicalScrap"), new ScrapAttributes(112.5f, 13.02f) },
+			//{ MyStringHash.GetOrCompute("MetalGridScrap"), new ScrapAttributes(15f, 1.82f) },
+			//{ MyStringHash.GetOrCompute("MotorScrap"), new ScrapAttributes(18.75f, 2.33f) },
+			//{ MyStringHash.GetOrCompute("PowerCellScrap"), new ScrapAttributes(9.75f, 1.44f) },
+			//{ MyStringHash.GetOrCompute("RadioCommunicationScrap"), new ScrapAttributes(6.75f, 1.08f) },
+			//{ MyStringHash.GetOrCompute("ReactorScrap"), new ScrapAttributes(30f, 7.34f) },
+			//{ MyStringHash.GetOrCompute("SmallTubeScrap"), new ScrapAttributes(3.75f, 0.48f) },
+			//{ MyStringHash.GetOrCompute("SolarCellScrap"), new ScrapAttributes(6.75f, 2.12f) },
+			//{ MyStringHash.GetOrCompute("SteelPlateScrap"), new ScrapAttributes(15.75f, 2f) },
+			//{ MyStringHash.GetOrCompute("SuperconductorScrap"), new ScrapAttributes(9f, 1.03f) },
+			//{ MyStringHash.GetOrCompute("ThrustScrap"), new ScrapAttributes(31.05f, 3.75f) },
 
 			// Mods
-			{ MyStringHash.GetOrCompute("FieldEmitterScrap"), new ScrapAttributes(92.25f, 14.92f) }
+			{ MyStringHash.GetOrCompute("FieldEmitterScrap"), new ScrapAttributes(0.217f, 0.595f) }
 		};
 
 		public static readonly Dictionary<MyStringHash, string> ComponentDictionary = new Dictionary<MyStringHash, string>
@@ -90,6 +112,19 @@ namespace AwwScrap.Support
 
 			// Mods
 			MyStringHash.GetOrCompute("ShieldComponentToIngot"),
+		};
+
+		public static readonly List<string> SKitScrapList = new List<string>()
+		{
+			"Scrap",
+			"ComputerScrap",
+			"ConstructionScrap",
+			"DisplayScrap",
+			"GirderScrap",
+			"InteriorPlateScrap",
+			"MotorScrap",
+			"SolarCellScrap",
+			"SteelPlateScrap"
 		};
 
 		public static readonly bool SkipTieredTech = true;
